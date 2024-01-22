7f9d4eff83efd0b94bdecd6f4968e00ca9118754
zyma <61933199+imhexagram@users.noreply.github.com>
2024-01-22 09:53:44 +0100

    Update TP_LWE.py

--- SR2I307 - Quantique architecture programmation et cryptographie/TP_LWE.py
+++ SR2I307 - Quantique architecture programmation et cryptographie/TP_LWE.py
@@ -10,7 +10,7 @@ def balance(e):
     return ZZ(e)-q if ZZ(e)>q/2 else ZZ(e)
 
 def keygen():
-    sA = matrix(Zq, n, 1, [randint(1, q) for _ in range(n)])%q
+    sA = matrix(Zq, n, 1, [randint(-beta, beta) for _ in range(n)])
     eA = matrix(Zq, m, 1, [randint(-beta, beta) for _ in range(m)])
     pKA = (A*sA + eA)%q
     return sA, eA, pKA
@@ -18,21 +18,25 @@ def keygen():
 
 def encrypt(mu, pkA):
     eB = matrix(Zq, n, 1, [randint(-beta, beta) for _ in range(n)])
-    sB = matrix(Zq, m, 1, [randint(1, q) for _ in range(m)])%q
+    sB = matrix(Zq, m, 1, [randint(-beta, beta) for _ in range(m)])
     pkB = (sB.T*A + eB.T)%q
     eB2 = randint(-beta, beta)
     c = (sB.T*pkA)[0][0] + eB2 + mu*floor(q/2)
     return pkB, c
 
 def decrypt(pkB, c, sA):
-    return abs(round(floor((q/2)**-1)*balance(c - (pkB*sA)[0][0])))
+    return abs(round((floor(q/2)**-1)*balance(c - (pkB*sA)[0][0])))
 
-for i in range(10):
+e0 = 0
+e1 = 0
+for i in range(1000):
     sA, eA, pkA = keygen()
     pkB, c = encrypt(0, pkA)
     if(0 != decrypt(pkB, c, sA)):
         print("erreur 0")
+        e0=e0+1
     pkB, c = encrypt(1, pkA)
     if(1 != decrypt(pkB, c, sA)):
         print("erreur 1")
-print("fin")
+        e1=e1+1
+print("fin. "+"erreur with 0: ",e0,", erreur with 1: ",e1)
