7f8c9de4bd240f27b8dc0aaa655abef9b56ade22
zyma <61933199+imhexagram@users.noreply.github.com>
2024-01-22 11:11:51 +0100

    Update TP_LWE.py

--- SR2I307 - Quantique architecture programmation et cryptographie/TP_LWE.py
+++ SR2I307 - Quantique architecture programmation et cryptographie/TP_LWE.py
@@ -26,7 +26,49 @@ def encrypt(mu, pkA):
 
 def decrypt(pkB, c, sA):
     return abs(round((floor(q/2)**-1)*balance(c - (pkB*sA)[0][0])))
+"""
+def balance_multi(e):
+    r = matrix(Zq, e.nrows(), e.ncols(), [0 for _ in range(e.nrows()*e.ncols())])
+    for i in range(0,e.nrows()):
+        for j in range(0,e.ncols()):
+            r[i][j] = ZZ(e[i][j])-q if ZZ(e[i][j])>q/2 else ZZ(e[i][j])
+    return r
+"""
+def balance_multi(e):
+    nrows, ncols = e.nrows(), e.ncols()
+    r = matrix(Zq, nrows, ncols, [[ZZ(e[i][j]) - q if ZZ(e[i][j]) > q/2 else ZZ(e[i][j]) for j in range(ncols)] for i in range(nrows)])
+    return r
 
+def keygen_multi():
+    sA = matrix(Zq, n, k, [randint(-beta, beta) for _ in range(n*k)])
+    eA = matrix(Zq, m, k, [randint(-beta, beta) for _ in range(m*k)])
+    pKA = (A*sA + eA)%q
+    return sA, eA, pKA
+
+
+def encrypt_multi(mu, pkA):
+    eB = matrix(Zq, n, k, [randint(-beta, beta) for _ in range(n*k)])
+    sB = matrix(Zq, m, k, [randint(-beta, beta) for _ in range(m*k)])
+    pkB = (sB.T*A + eB.T)%q
+    eB2 = matrix(Zq, k, k, [randint(-beta, beta) for _ in range(k*k)])
+    c = (sB.T*pkA) + eB2 + mu*floor(q/2)
+    return pkB, c
+
+def decrypt_multi(pkB, c, sA):
+    return abs(round((floor(q/2)**-1)*balance_multi(c - (pkB*sA))))
+
+e = 0
+k = 10
+for i in range(10):
+    sA, eA, pkA = keygen_multi()
+    mu = matrix(Zq, k, k, [randint(0, 1) for _ in range(k*k)])
+    pkB, c = encrypt_multi(mu, pkA)
+    if(mu != decrypt_multi(pkB, c, sA)):
+        print("erreur")
+        e=e+1
+print("fin. "+"erreur : ",e)
+
+"""
 e0 = 0
 e1 = 0
 for i in range(1000):
@@ -40,3 +82,4 @@ for i in range(1000):
         print("erreur 1")
         e1=e1+1
 print("fin. "+"erreur avec 0: ",e0,", erreur avec 1: ",e1)
+"""
